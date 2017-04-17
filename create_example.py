4a7e60072937a233d868e3e5194dd13777ced4a8
Aldo Mendez <aldomendez@berkeley.edu>
2017-04-17 13:36:06 -0700

    added .in files, creates scripts

---
+++ create_example.py
@@ -0,0 +1,51 @@
+import random
+import sys
+
+problem_num = sys.argv[1]
+MAX_POUNDS = 2**(32) - 1 
+MAX_MONEY = 2**(32) - 1
+MAX_N = 100000 #200,000 number of items
+MAX_C = 150000 #200,000 number of constraints
+name = "problem"
+rest_name = ".in"
+items = []
+constraints = []
+classes = [i for i in range(0,1000)]
+
+#create random items of class 1-1000 (can tweak parameters)
+for i in range(1, int(MAX_N/4)):
+    c = random.choice(classes)
+    weight = 1000*random.random()
+    cost = 1000*random.random()
+    resale = cost + 100*random.random()
+    temp = "item" + str(i) + "; " + str(c) + "; "
+    temp += str(round(weight,2)) + "; " + str(round(cost,2)) + "; " +str(round(resale,2))
+    items.append(temp)
+
+#create random constraints
+for i in range(0,int(MAX_C/4)):
+    num_c = int(5*random.random())
+    temp = ""
+    for i in range(num_c):
+        temp += str(random.choice(classes)) + ", "
+    temp = temp[:-2]
+    constraints.append(temp)
+
+#create/open file
+f = open(name + problem_num + rest_name, "w+")
+f.write(str(MAX_POUNDS) + "\n")
+f.write(str(MAX_MONEY) + "\n")
+f.write(str(MAX_N) + "\n")
+f.write(str(MAX_N) + "\n")
+f.write(str(MAX_C) + "\n")
+
+#write items
+for item in items:
+    f.write(item)
+    f.write("\n")
+#for loop, write constraints
+for constraint in constraints:
+    f.write(constraint)
+    f.write("\n")
+
+f.close()
---
+++ create_examples.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 create_example.py 1
+python3 create_example.py 2
+python3 create_example.py 3
