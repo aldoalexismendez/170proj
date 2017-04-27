076dd74aa4cb215ce4ea99023f79976ba6e79065
Aldo Mendez <aldomendez@berkeley.edu>
2017-04-27 06:42:32 +0200

    just need indep. set

--- submission_folder/code/pick_items_solver_greedy.py
+++ submission_folder/code/pick_items_solver_greedy.py
@@ -1,12 +1,18 @@
 import os
 import numpy as np
+import pq
+import graph
 
 DIRECTORY = "../../project_instances/"
+OUTPUT_DIRECTORY = "../output/"
 
 for file_name in os.listdir(DIRECTORY):
+    #Ensures that only .in files are opened
     if not file_name.endswith(".in"):
         continue
     file_obj = open(DIRECTORY + file_name, "r")
+    
+    #Set variables
     MAX_POUNDS = round(float(file_obj.readline()),2)
     MAX_MONEY = round(float(file_obj.readline()),2)
     N = int(file_obj.readline())
@@ -14,6 +20,10 @@ for file_name in os.listdir(DIRECTORY):
     items = []
     constraints = []
     constraint_matrix = np.ones((N,N))
+    p = pq.PriorityQueue()#keeps track of best items
+    total_money = 0
+    total_weight = 0
+    items_taken = []
     for i in range(N):
         temp = file_obj.readline().split(";")
         temp[-1] = temp[-1][:-1]
@@ -30,5 +40,22 @@ for file_name in os.listdir(DIRECTORY):
                     constraint_matrix[j][k] = 0
         constraints.append(temp)
 
+    #Create independent set
+    #place items of independent set into PQ based on weight/profit ratio
+    for i in range(5):
+        item = items[i]
+        ratio = float(item[2])/float(item[3])
+        p.push(item, ratio)
 
-    
+    #Greedy part
+    while total_money <= MAX_MONEY and total_weight <= MAX_POUNDS and not p.isEmpty():
+        item = p.pop()
+        items_taken.append(item[0])
+        total_weight += float(item[2])
+        total_money += float(item[3])
+
+    #Create output file
+    f = open(OUTPUT_DIRECTORY + file_name[:-3] + ".out", "w+")
+
+    for item in items_taken:
+        f.write(item + "\n")
